7dce5be39efefbe81f10119f690a4929265acef3
jasiel becerra <jasiel.csn@outlook.com>
2021-09-03 10:29:11 -0500

    Agregar archivos de proyecto.

---
+++ Server/Services/TenantService.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using MultitenancyBlazor.Shared.ConfiguracionTenant;
+using MultitenancyBlazor.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultitenancyBlazor.Server.Services
+{
+    public class TenantService : ITenantService
+    {
+        private readonly TenantSettings _tenantSettings;
+        private HttpContext _httpContext;
+        private Tenant _currentTenant;
+
+        public TenantService(IOptions<TenantSettings> tenantSettings, IHttpContextAccessor contextAccessor)
+        {
+            _tenantSettings = tenantSettings.Value;
+            _httpContext = contextAccessor.HttpContext;
+            if (_httpContext != null)
+            {
+                if (_httpContext.Request.Headers.TryGetValue("tenant", out var tenantId))
+                {
+                    SetTenant(tenantId);
+                }
+                else
+                {
+                    throw new Exception("Invalid Tenant!");
+                }
+            }
+        }
+
+        private void SetTenant(string tenantId)
+        {
+            _currentTenant = _tenantSettings.Tenants.Where(a => a.TID == tenantId).FirstOrDefault();
+            if (_currentTenant == null) throw new Exception("Invalid Tenant!");
+            if (string.IsNullOrEmpty(_currentTenant.ConnectionString))
+            {
+                SetDefaultConnectionStringToCurrentTenant();
+            }
+        }
+
+        private void SetDefaultConnectionStringToCurrentTenant()
+        {
+            _currentTenant.ConnectionString = _tenantSettings.Defaults.ConnectionString;
+        }
+
+        public string GetConnectionString()
+        {
+            return _currentTenant?.ConnectionString;
+        }
+
+        public string GetDatabaseProvider()
+        {
+            return _tenantSettings.Defaults?.DBProvider;
+        }
+
+        public Tenant GetTenant()
+        {
+            return _currentTenant;
+        }
+    }
+}
---
+++ Shared/Contratos/IMustHaveTenant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultitenancyBlazor.Shared.Contratos
+{
+    public interface IMustHaveTenant
+    {        
+        public string TenantId { get; set; }
+    }
+}
